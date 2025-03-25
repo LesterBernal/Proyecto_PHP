28bc531b2188e7cae51f912bee9e3450b1685493
Lester Bernal <bernalpinedalesteralejandro@gmail.com>
2025-03-25 00:01:05 +0000

    Commit Iniciar con Codigo PHP

---
+++ hola.php
@@ -0,0 +1,17 @@
+<?php
+// Definir el nombre del estudiante
+$nombre = "Lester Bernal";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mundo en PHP</title>
+</head>
+<body>
+    <h1><?php echo "Hola mundo, soy $nombre"; ?></h1>
+    <img src="/Actividad3/ProyectoPhp/Imagen_IA_Lester.jpeg" alt="Imagen de ejemplo" width="300">
+</body>
+</html>
